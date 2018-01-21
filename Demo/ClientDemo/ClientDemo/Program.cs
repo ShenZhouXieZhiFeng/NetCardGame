69c012a31c34c94ebc8a8bf67a429a3ccf67ef90
Stone <827443469@qq.com>
2018-01-21 14:09:17 +0800

    ScoketDemo

---
+++ Demo/ClientDemo/ClientDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ClientDemo
+{
+    class Program
+    {
+        private static Socket clientSocket = null;
+        static void Main(string[] args)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint remoteIp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            clientSocket.Connect(remoteIp);
+            Console.WriteLine("服务器连接成功");
+
+            byte[] res = new byte[1024];
+            int len = clientSocket.Receive(res);
+            Console.WriteLine(Encoding.Default.GetString(res, 0, len));
+
+            clientSocket.Send(Encoding.Default.GetBytes("服务端你好"));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Demo/SocketDemo/SocketDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketDemo
+{
+    class Program
+    {
+
+        static Socket serverSocket = null;
+
+        static void Main(string[] args)
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 9999);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+            Thread thread = new Thread(listenClientConnectThread);
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 监听客户端连接
+        /// </summary>
+        static void listenClientConnectThread()
+        {
+            Socket client = serverSocket.Accept();
+            Console.WriteLine("客户端连接成功");
+            client.Send(Encoding.Default.GetBytes("客户端你好！"));
+            Thread receive = new Thread(receiveClientThread);
+            receive.Start(client);
+        }
+
+        /// <summary>
+        /// 接收客户端消息
+        /// </summary>
+        static void receiveClientThread(object _client)
+        {
+            Socket client = (Socket)_client;
+            byte[] recs = new byte[1024];
+            int length = client.Receive(recs);
+            Console.WriteLine(Encoding.Default.GetString(recs, 0, length));
+        }
+    }
+}
